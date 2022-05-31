51b1b2cc5e0d3f268323dc418b7cae1cd7513cef
luanntph25463 <105334790+luanntph25463@users.noreply.github.com>
2022-05-31 12:46:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+document.querySelector("#app").innerHTML = `
+  <h1 class="bg-red-500">Hello luan!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`;
